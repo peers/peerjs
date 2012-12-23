0b45054bf68d7eace14cdaa3b5f88d08c07cf52c
Michelle Bu <analogmidnight@gmail.com>
2012-12-23 00:01:31 -0800

    all works

--- public/js/sink.js
+++ public/js/sink.js
@@ -23,22 +23,28 @@ SinkPeer.prototype.socketInit = function(cb) {
 
     self._socket.on('offer', function(offer) {
       self._pc.setRemoteDescription(offer.sdp, function() {
-        self._pc.createAnswer(function(answer) {
-          self._pc.setLocalDescription(answer, function() {
-            self._socket.emit('answer',
-                { 'sink': self._id,
-                  'sdp': answer,
-                  'source': offer.source });
-            // Firefoxism
-            console.log('FIREFOX');
-            self._pc.connectDataConnection(5001, 5000);
-            console.log('FIREFOX-2');
+
+        //Firefox
+        navigator.mozGetUserMedia({ audio: true, fake: true }, function(s) {
+          self._pc.addStream(s);
+
+          self._pc.createAnswer(function(answer) {
+            self._pc.setLocalDescription(answer, function() {
+              self._socket.emit('answer',
+                  { 'sink': self._id,
+                    'sdp': answer,
+                    'source': offer.source });
+              // Firefoxism
+              console.log('FIREFOX');
+              self._pc.connectDataConnection(5001, 5000);
+              console.log('FIREFOX-2');
+            }, function(err) {
+              console.log('failed to setLocalDescription, ', err)
+            });
           }, function(err) {
-            console.log('failed to setLocalDescription, ', err)
+            console.log('failed to create answer, ', err)
           });
-        }, function(err) {
-          console.log('failed to create answer, ', err)
-        });
+        }, function(err) { console.log('crap'); });
       }, function(err) {
         console.log('failed to setRemoteDescription with offer, ', err);
       });
