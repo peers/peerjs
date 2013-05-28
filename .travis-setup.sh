ee869b03c4d50066c517615be7b00c656ab4deb7
Sergey Klimov <sergey.v.klimov@gmail.com>
2013-05-27 19:11:45 -0700

    browser install

---
+++ .travis-setup.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+# Make sure /dev/shm has correct permissions.
+ls -l /dev/shm
+sudo chmod 1777 /dev/shm
+ls -l /dev/shm
+
+
+case $BROWSER in
+Chrome*)
+	export VERSION=$(echo $BROWSER | sed -e's/[^-]*-//')
+	export BROWSER=$(echo $BROWSER | sed -e's/-.*//')
+	echo "Getting $VERSION of $BROWSER"
+	export CHROME=google-chrome-${VERSION}_current_amd64.deb
+	wget https://dl.google.com/linux/direct/$CHROME
+	sudo dpkg --install $CHROME || sudo apt-get -f install
+	ls -l /usr/bin/google-chrome
+	google-chrome --version
+	;;
+
+Firefox)
+	firefox --version
+	;;
+esac
+
