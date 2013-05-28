897cd69a9cf6dbdb3ee36caf5a4f786d4d738712
Sergey Klimov <sergey.v.klimov@gmail.com>
2013-05-27 19:24:40 -0700

    travis build fixes

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -8,18 +8,19 @@ ls -l /dev/shm
 
 case $BROWSER in
 Chrome*)
-	export VERSION=$(echo $BROWSER | sed -e's/[^-]*-//')
-	export BROWSER=$(echo $BROWSER | sed -e's/-.*//')
-	echo "Getting $VERSION of $BROWSER"
-	export CHROME=google-chrome-${VERSION}_current_amd64.deb
-	wget https://dl.google.com/linux/direct/$CHROME
-	sudo dpkg --install $CHROME || sudo apt-get -f install
-	ls -l /usr/bin/google-chrome
-	google-chrome --version
-	;;
+    export VERSION=$(echo $BROWSER | sed -e's/[^-]*-//')
+    export BROWSER=$(echo $BROWSER | sed -e's/-.*//')
+    echo "Getting $VERSION of $BROWSER"
+    export CHROME=google-chrome-${VERSION}_current_amd64.deb
+    wget https://dl.google.com/linux/direct/$CHROME
+    sudo dpkg --install $CHROME || sudo apt-get -f install
+    ls -l /usr/bin/google-chrome
+    google-chrome --version
+    export CHROME_BIN=/usr/bin/google-chrome --no-sandbox
+    ;;
 
 Firefox)
-	firefox --version
-	;;
+    firefox --version
+    ;;
 esac
 
