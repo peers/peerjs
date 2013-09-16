eee65a8530442a8f263ce58b479a2700edda2e8c
Michelle Bu <michelle@michellebu.com>
2013-09-16 00:13:41 -0700

    API docs pretty much done

--- docs/build.js
+++ docs/build.js
@@ -5,4 +5,4 @@ var reference = require('reference');
 var file = fs.readFileSync('./api.json');
 
 var template = handlebars.compile(fs.readFileSync('./template.html', {encoding: 'utf8'}));
-fs.writeFile('./index.html', template({html: reference(file)}));
+fs.writeFile('./index.html', template({html: reference(file, {anchor: true})}));
