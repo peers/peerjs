da26250e7249bcab5ea002a60047974054d0a0a4
Michelle Bu <michelle@michellebu.com>
2013-09-17 21:38:05 -0700

    most styles are done

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   var $api = $('.api');
   var $start = $('.start');
-  var $show = $('.api .show');
-  var $hide = $('.api .hide');
+  var $show = $('.left .show');
+  var $hide = $('.left .hide');
   var width = $(window).width();
   var height = $(window).height();
   var THRESHOLD = 700;
@@ -20,9 +20,13 @@ $(document).ready(function() {
     if (width < THRESHOLD) {
       $api.addClass('fullscreen');
       $start.addClass('full');
+      $show.hide();
+      $hide.hide();
     } else {
       $api.removeClass('fullscreen');
       $start.removeClass('full');
+      $show.show();
+      $hide.show();
     }
 
     if ($api.attr('class').indexOf('hidden') === -1) {
@@ -36,26 +40,32 @@ $(document).ready(function() {
     $api.addClass('hidden');
     if (width >= THRESHOLD) {
       $start.addClass('full');
+      $hide.hide();
+      $show.show();
     }
     $start.removeClass('hidden');
-    $hide.hide();
-    $show.show();
   }
 
   function showAPI() {
     $api.removeClass('hidden');
     if (width >= THRESHOLD) {
       $start.removeClass('full');
+      $show.hide();
+      $hide.show();
     }
     $start.addClass('hidden');
-    $show.hide();
-    $hide.show();
   }
 
-  $('body').on('click', '.hide', function() {
-    hideAPI();
+  $('body').on('click', '.left', function() {
+    if ($api.attr('class').indexOf('hidden') !== -1) {
+      showAPI();
+    } else if ($api.attr('class').indexOf('fullscreen') === -1) {
+      // Now the headers are only links.
+      console.log($api.attr('class'));
+      hideAPI();
+    }
   });
-  $('body').on('click', '.hidden', function() {
-    showAPI();
+  $('body').on('click', '.right', function() {
+    hideAPI();
   });
 });
