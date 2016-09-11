2b2e51bc2fdeb699446bdc0b4ab47cbd29b42560
SupremeTechnopriest <randylebeau@gmail.com>
2016-09-11 17:02:58 -0400

    build

--- dist/peer.js
+++ dist/peer.js
@@ -1415,7 +1415,7 @@ Socket.prototype.close = function() {
 module.exports = Socket;
 
 },{"./util":8,"eventemitter3":9}],8:[function(require,module,exports){
-var defaultConfig = {'iceServers': [{ 'url': 'stun:stun.l.google.com:19302' }]};
+var defaultConfig = {'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }]};
 var dataCount = 1;
 
 var BinaryPack = require('js-binarypack');
