0c77a5beb6e8d0b3edf3e6b0e8c5844012c20f9c
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2018-10-26 01:34:07 +0200

    feat(typescript): First commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,19 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
 
     browserify: {
       dev: {
-        src: ['lib/exports.js'],
-        dest: 'dist/peer.js'
+        src: ["dist/exports.js"],
+        dest: "dist/peer.js"
       }
     },
 
     uglify: {
       prod: {
         options: { mangle: true },
-        src: 'dist/peer.js',
-        dest: 'dist/peer.min.js'
+        src: "dist/peer.js",
+        dest: "dist/peer.min.js"
       }
     },
 
@@ -21,8 +21,8 @@ module.exports = function (grunt) {
       dev: {
         files: [
           {
-            src: 'dist/peer.min.js',
-            dest: 'test/public/peer.min.js'
+            src: "dist/peer.min.js",
+            dest: "./test/public/peer.min.js"
           }
         ]
       }
@@ -30,20 +30,36 @@ module.exports = function (grunt) {
 
     concat: {
       dev: {
-        src: 'dist/peer.js',
-        dest: 'dist/peer.js',
+        src: "dist/peer.js",
+        dest: "dist/peer.js"
       },
       prod: {
-        src: 'dist/peer.min.js',
-        dest: 'dist/peer.min.js',
+        src: "dist/peer.min.js",
+        dest: "dist/peer.min.js"
+      }
+    },
+    ts: {
+      default: {
+        src: ["./lib/**/*.ts", "!node_modules/**"],
+        tsconfig: "./tsconfig.json"
+      },
+      options: {
+        failOnTypeErrors: false
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-ts");
 
-  grunt.registerTask('default', ['browserify', 'uglify', 'concat', 'copy']);
-}
+  grunt.registerTask("default", [
+    "ts",
+    "browserify",
+    "uglify",
+    "concat",
+    "copy"
+  ]);
+};
