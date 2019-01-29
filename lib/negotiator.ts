9415f90c591cc182fd482ef89f496457447a133f
Jairo Caro-Accino <kidandcat@gmail.com>
2019-01-29 14:31:37 +0100

    disconnected may be a transient state

--- lib/negotiator.ts
+++ lib/negotiator.ts
@@ -164,7 +164,6 @@ Negotiator._setupListeners = function(connection, pc, pc_id) {
         util.log(
           "iceConnectionState is disconnected, closing connections to " + peerId
         );
-        connection.close();
         break;
       case "completed":
         pc.onicecandidate = util.noop;
