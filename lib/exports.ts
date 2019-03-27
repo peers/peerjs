54bcd8b51b71de12c5e4f530cfdc56cfb180593d
afrokick <devbyru@gmail.com>
2019-03-27 20:04:04 +0300

    refactored according https://github.com/peers/peerjs/issues/157

--- lib/exports.ts
+++ lib/exports.ts
@@ -1,25 +1,15 @@
 import { util } from "./util";
-import {
-  RTCPeerConnection,
-  RTCSessionDescription,
-  RTCIceCandidate
-} from "./adapter";
-import { Socket } from "./socket";
 import { MediaConnection } from "./mediaconnection";
 import { DataConnection } from "./dataconnection";
 import { Peer } from "./peer";
-import Negotiator from "./negotiator";
-import jsBinarypack from "js-binarypack";
 
-window.Socket = Socket;
-window.MediaConnection = MediaConnection;
-window.DataConnection = DataConnection;
-window.Peer = Peer;
-window.RTCPeerConnection = RTCPeerConnection;
-window.RTCSessionDescription = RTCSessionDescription;
-window.RTCIceCandidate = RTCIceCandidate;
-window.Negotiator = Negotiator;
-window.util = util;
-window.BinaryPack = jsBinarypack;
+export const peerjs = {
+  MediaConnection,
+  DataConnection,
+  Peer,
+  util
+};
 
 export default Peer;
+
+(<any>window).peerjs = peerjs;
