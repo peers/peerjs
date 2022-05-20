07b4ef6f6286cc4afc9a9c9615dd7440ff0a790d
Joel Wetzell <jwetzell@yahoo.com>
2022-05-20 10:51:47 -0500

    add type exports

--- lib/exports.ts
+++ lib/exports.ts
@@ -10,6 +10,16 @@ export type {
 export type { UtilSupportsObj } from "./util";
 export type { DataConnection } from "./dataconnection";
 export type { MediaConnection } from "./mediaconnection";
+export type { LogLevel } from './logger'
+export type {
+	ConnectionEventType,
+	ConnectionType,
+	PeerEventType,
+	PeerErrorType,
+	SerializationType,
+	SocketEventType,
+	ServerMessageType,
+} from "./enums";
 
 export { Peer, util };
 export default Peer;
