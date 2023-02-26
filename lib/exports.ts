68ff68d1f9a501bddcae3edfca4557f3f3753a2a
Jonas Gloning <34194370+jonasgloning@users.noreply.github.com>
2023-02-26 19:29:29 +0100

    docs: document `DataConnection` inline

--- lib/exports.ts
+++ lib/exports.ts
@@ -9,7 +9,7 @@ export type {
 	CallOption,
 } from "./optionInterfaces";
 export type { UtilSupportsObj } from "./util";
-export type { DataConnection } from "./dataconnection";
+export type { DataConnection, DataConnectionEvents } from "./dataconnection";
 export type { MediaConnection, MediaConnectionEvents } from "./mediaconnection";
 export type { LogLevel } from "./logger";
 export type {
