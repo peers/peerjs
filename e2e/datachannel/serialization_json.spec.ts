5abd76a5b71056a0f7574e0eb8280f8ff1c31fe5
Jonas Gloning <34194370+jonasgloning@users.noreply.github.com>
2023-08-06 15:49:20 +0200

    test(e2e): retry flaky tests

--- e2e/datachannel/serialization_json.spec.ts
+++ e2e/datachannel/serialization_json.spec.ts
@@ -2,20 +2,43 @@ import P from "./serialization.page.js";
 import { serializationTest } from "./serializationTest.js";
 
 describe("DataChannel:JSON", () => {
-	beforeAll(async () => {
-		await P.init();
-	});
-	it("should transfer numbers", serializationTest("./numbers", "json"));
-	it("should transfer strings", serializationTest("./strings", "json"));
+	beforeAll(
+		async () => {
+			await P.init();
+		},
+		jasmine.DEFAULT_TIMEOUT_INTERVAL,
+		2,
+	);
+	it(
+		"should transfer numbers",
+		serializationTest("./numbers", "json"),
+		jasmine.DEFAULT_TIMEOUT_INTERVAL,
+		2,
+	);
+	it(
+		"should transfer strings",
+		serializationTest("./strings", "json"),
+		jasmine.DEFAULT_TIMEOUT_INTERVAL,
+		2,
+	);
 	// it("should transfer long string", serializationTest("./long_string", "json"));
-	it("should transfer objects", serializationTest("./objects", "json"));
+	it(
+		"should transfer objects",
+		serializationTest("./objects", "json"),
+		jasmine.DEFAULT_TIMEOUT_INTERVAL,
+		2,
+	);
 	it(
 		"should transfer arrays (no chunks)",
 		serializationTest("./arrays_unchunked", "json"),
+		jasmine.DEFAULT_TIMEOUT_INTERVAL,
+		2,
 	);
 	it(
 		"should transfer Dates as strings",
 		serializationTest("./dates_as_json_string", "json"),
+		jasmine.DEFAULT_TIMEOUT_INTERVAL,
+		2,
 	);
 	// it("should transfer ArrayBuffers", serializationTest("./arraybuffers", "json"));
 	// it("should transfer TypedArrayView", serializationTest("./typed_array_view", "json"));
